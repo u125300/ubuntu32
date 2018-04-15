235cd53a6dff8df7c15a60acd5c1adc91c33538e
u125300 <u125300@gmail.com>
2018-04-15 13:42:09 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install wget curl tar zip gzip build-essential libssl-dev uml-utilities 
+dir
+cd /MyProgram
+wget https://github.com/bambusoft/ntvl/releases/download/v1.0.0/bambusoft-ntvl-1.0.0.tar.gz
+tar -zxvf bambusoft-ntvl-1.0.0.tar.gz
+cd bambusoft-ntvl-1.0.0
+make
+tar -czvf ntvl_32.tar.gz *
+curl -T ntvl_32.tar.gz -u yzcmjsn@gmail.com:0339109523 https://dav.box.com/dav/NTVL/ubuntu1/
